80d3c70e0bccec737abc372b66b10a056f798f02
Fariyad Husain <fariyad786husain@gmail.com>
2025-08-25 12:16:57 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Addition of two numbers in Python
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add
+sum_result = num1 + num2
+
+# Output
+print("The sum is:", sum_result)
